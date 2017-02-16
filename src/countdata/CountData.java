49f3726f5905a912b06e76d433be1e3b7074762a
Kush Gautam <Kush Gautam>
2017-02-17 01:19:45 +0545

    some comment removed! :p

--- src/countdata/CountData.java
+++ src/countdata/CountData.java
@@ -1,9 +1,4 @@
- /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package countdata;
+ package countdata;
 import java.util.Scanner;
 /**
  *
@@ -11,9 +6,6 @@ import java.util.Scanner;
  */
 public class CountData {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         System.out.println("Program that takes a sentence from user and counts no. of words and no. of characters.");
