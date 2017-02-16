a4231838cc813674ed3c23f67f854f256af0e34a
Kush Gautam <Kush Gautam>
2017-02-17 00:59:38 +0545

    Issue Solved! #1

--- src/countdata/CountData.java
+++ src/countdata/CountData.java
@@ -23,16 +23,21 @@ public class CountData {
         String sentence = scan.nextLine();
         System.out.println("Your sentence: " + sentence);
         
-        int wordCount = 1;
+        int wordCount = 0;
+        boolean word = false;
+        int endOfLine = sentence.length() - 1;
         for (int i = 0; i < sentence.length(); i++)
         {
-           if(sentence.charAt(i) == ' ' && sentence.charAt(i+1)!=' ')
-           {
+           if(Character.isLetter(sentence.charAt(i)) && i != endOfLine) {
+               word = true;
+           } else if (!Character.isLetter(sentence.charAt(i)) && word) {
+               wordCount++;
+               word = false;
+           } else if (Character.isLetter(sentence.charAt(i)) && i == endOfLine) {
                wordCount++;
            }
         }
         System.out.println("No. of word: " + wordCount);
         System.out.println("No. of character: " + sentence.length());
     }
-    
 }
